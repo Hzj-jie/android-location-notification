c9b8106571b5b7cd8b3b830ba6351ee6ea0ab151
Hzj_jie <hzj_jie@hotmail.com>
2017-10-31 00:59:38 -0700

    NotifyService

---
+++ src/main/java/org/gemini/location_notification/NotifyService.java
@@ -0,0 +1,10 @@
+package org.gemini.location_notification;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import org.gemini.shared.KeepAliveService;
+
+public final class NotifyService extends KeepAliveService {
+}
